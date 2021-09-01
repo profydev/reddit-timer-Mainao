153040900001ba16d87c170954f6915fd5b1bd1a
Mainao Boro <mainaoboro@Mainaos-MacBook-Air.local>
2021-09-02 01:44:55 +0530

    added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/search" component={Search} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <p>App placeholder</p>;
+
+export default Home;
