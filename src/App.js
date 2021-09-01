70351ba3f6772e923e1e134fd6232e639953a7c1
Mainao Boro <mainaoboro@Mainaos-MacBook-Air.local>
2021-09-02 03:20:56 +0530

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
 import Home from './pages/Home';
 import Search from './pages/Search';
 
 function App() {
   return (
     <Router>
+      <Normalize />
+      <GlobalStyle />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/search" component={Search} />
